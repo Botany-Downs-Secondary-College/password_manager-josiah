c5a0281f104dbc4e40aef0759ce15652473966e2
JosiahKitchen <78832319+JosiahKitchen@users.noreply.github.com>
2021-03-10 14:53:15 +1300

    Add files via upload

--- password_manager.py
+++ password_manager.py
@@ -9,10 +9,19 @@ name_list =[]
 loop1 = 1
 loop2 = 2
 attempts = 0
-name_storage = ''
+name_dict = {}
+name_count = 0
+access_dictionary = {}
+
+#Name - dict or string?
+#First time, list/dict empty
 
 def view_list(name):
-    print(account_list[account_list.index(name)])
+    try:
+        print(account_list[account_list.index(name)])
+    except:
+        print("You have no passwords in your list")
+        
     remove_determine = input("Press Enter to continue or type 'Remove' to remove an object from the list")
     if remove_determine == "":
         menu(name)
@@ -39,15 +48,20 @@ def search(name):
                print("This website could not be found, check your spelling and try again")
 
 def Password_save(name, site, password):
-    name[site] = password
-    if name in account_list:
-        pass
+    global name_dict
+    global access_dictionary
+    temp_name = []
+    temp_name = [name, {site:password}]
+    if temp_name or access_dictionary[name] in account_list:
+        #Hopefully this works
+        account_list[account_list.index([name, {}])[2]][site] = password
+        access_dictionary[name] = {}.append(site, password)
     else:
-        account_list.append(name)
+        account_list.append(temp_name)
 
 def menu(name):
     loop2 = 1
-    global name_storage
+    #global name_storage
     while loop2 == 1:
         menu = int(input("Type 1 to view your password list, Type 2 to search, Type 3 to add password, Type 4 to exit/log out "))
         if menu == 1:
@@ -65,16 +79,22 @@ def menu(name):
 def create_account():
     global account_list
     global name_list
+    global loop1
+    global name_dict
+    global name_count
     name = input("Please enter your name ")
     age = int(input("Please enter your age "))
     if age >= 13 and age <= 125:
+        name_count += 1
+        name_dict[name] = name_count
         password = input("Enter your password ")
         name_list.append([name, password])
         print("Account created")
         menu(name)
         
     else:
-        pass #To be changed
+        print("You do not meet the age requirement")
+        loop1 = 5
         
 
 def log_in():
@@ -98,7 +118,7 @@ def log_in():
         
 print("Welcome")
 while loop1 == 1:
-    entry = ("Type 1 to log in, Type 2 to create an account ")
+    entry = input("Type 1 to log in, Type 2 to create an account ")
     if int(entry) == 1:
         log_in()
                 
